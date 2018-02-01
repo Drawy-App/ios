260d9e0f46902e410fcda88a9f78299196e67457
Alexey Landyrev <landyrev@yandex-team.ru>
2018-02-01 20:38:23 +0300

    woff

--- htd/Screens/RecognizerViewController.swift
+++ htd/Screens/RecognizerViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 import Metal
 import AVFoundation
 import Vision
+import CoreGraphics
 
 class RecognizerViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    @IBOutlet weak var countLabel: UILabel!
-    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var previewView: UIImageView!
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var request: VNCoreMLRequest?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initModel()
         initCamera()
+        try? VNImageRequestHandler(
+                ciImage: CIImage.init(image: UIImage.init(named: "test")!)!
+            ).perform([request!])
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,21 +33,40 @@ class RecognizerViewController: UIViewController, AVCaptureVideoDataOutputSample
         // Dispose of any resources that can be recreated.
     }
     
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let model = try? VNCoreMLModel(for: my_model().model) else {return}
-        
-        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
-            let piss = finishedRequest.results as! [VNCoreMLFeatureValueObservation]
-            let value = piss[0].featureValue.multiArrayValue![0] as! Double
-            DispatchQueue.main.async(execute: {
-                self.countLabel.text = String.init(format: "%f", value)
-            })
+    func initModel() {
+        guard let model = try? VNCoreMLModel(for: realmodel().model) else {return}
+        request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            let results = finishedRequest.results as! [VNClassificationObservation]
+            print(results.first!.identifier, Int(results.first!.confidence * 100))
         }
-        
+        request!.imageCropAndScaleOption = .scaleFill
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        CVPixelBufferLockBaseAddress(pixelBuffer, .readOnly);
+        var baseImg = CIImage.init(cvPixelBuffer: pixelBuffer)
+        baseImg = cropCiImage(baseImg)
+        DispatchQueue.main.async {
+            self.previewView.image = UIImage.init(ciImage: baseImg)
+        }
+        try? VNImageRequestHandler(ciImage: baseImg, options: [:]).perform([request!])
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, .readOnly);
+
+//
+//        // executes request
+//        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request!])
+    }
+    
+    func cropCiImage(_ ciImage: CIImage) -> CIImage {
+        var newImage = ciImage
+        newImage = newImage.oriented(.right)
+        let imageSize = newImage.extent
+        let targetSize = imageSize.width > imageSize.height ? imageSize.height : imageSize.width
+        let targetRect = CGRect.init(x: 0, y: 0, width: targetSize, height: targetSize)
         
-        // executes request
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        return newImage.cropped(to: targetRect)
+        
     }
     
     func initCamera() {
@@ -51,10 +75,10 @@ class RecognizerViewController: UIViewController, AVCaptureVideoDataOutputSample
         do {
             let input = try AVCaptureDeviceInput(device: captureDevice)
             let output = AVCaptureVideoDataOutput()
-//            output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
             output.alwaysDiscardsLateVideoFrames = true
-            output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String:kCVPixelFormatType_32BGRA]
-            
+            print("available", output.availableVideoPixelFormatTypes)
+            output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String:output.availableVideoPixelFormatTypes[2]]
+            print(output.videoSettings)
             
             let queue = DispatchQueue(label: "main")
             output.setSampleBufferDelegate(self, queue: queue)
@@ -72,10 +96,12 @@ class RecognizerViewController: UIViewController, AVCaptureVideoDataOutputSample
                 print(error)
             }
             
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
-            videoPreviewLayer?.videoGravity = .resize
-            videoPreviewLayer?.frame = view.layer.bounds
-            previewView.layer.addSublayer(videoPreviewLayer!)
+//            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+//            videoPreviewLayer!.videoGravity = .resizeAspectFill
+//            videoPreviewLayer!.frame = CGRect.init(x: 0, y: 0, width: 375, height: 375)
+//            previewView.layer.addSublayer(videoPreviewLayer!)
+            
+            
             
             captureSession?.startRunning()
         } catch {
